2efe9261f331d4041527f659b394b59b55f6546a
tmato <tmato@politicr.net>
2017-01-22 10:12:36 +0100

    updated services

---
+++ RSSFeedAggregator/app/src/main/java/com/timotheemato/rssfeedaggregator/network/exceptions/NetworkGeneralException.java
@@ -0,0 +1,8 @@
+package com.timotheemato.rssfeedaggregator.network.exceptions;
+
+/**
+ * Created by tmato on 1/22/17.
+ */
+
+public class NetworkGeneralException extends IllegalArgumentException {
+}
